055154cc447c7532c2cb71320c898d28f3e51702
supun <supunsandaruwan000000@gmail.com>
2021-10-30 03:18:07 +0530

    paragraph line added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
 
 function App() {
-  const value = process.env.REACT_APP_API_KEY;
   return (
     <div className="App">
       <h1>Hello from CI/CD app</h1>
-      {`VALUE IS: ${value}`}
-      <p>Added new Line</p>
+      {`VALUE IS: ${process.env.REACT_APP_API_KEY}`}
+      <p>Added paragraph line </p>
     </div>
   );
 }
