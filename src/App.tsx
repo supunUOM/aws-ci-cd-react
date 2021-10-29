7b513a0e8d879c56d1d7e44d37b94cf259267026
supun <supunsandaruwan000000@gmail.com>
2021-10-30 01:05:50 +0530

    changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import dotenv from 'dotenv';
-dotenv.config();
 
 function App() {
-  const value = process.env.API_KEY;
-  return <div className="App">
-    <h1>Hello from CI/CD app</h1>
-    {`VALUE IS: ${value}`}</div>;
+  const value = process.env.REACT_APP_API_KEY;
+  return (
+    <div className="App">
+      <h1>Hello from CI/CD app</h1>
+      {`VALUE IS: ${value}`}
+    </div>
+  );
 }
 
 export default App;
