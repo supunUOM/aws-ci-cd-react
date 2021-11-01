a91d1c346ad5cbfa9d84ca03f00e546e823caf7e
supun <supunsandaruwan000000@gmail.com>
2021-11-01 11:25:39 +0530

    screat key changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello from CI/CD app</h1>
-      {`VALUE IS: ${process.env.REACT_APP_API_KEY}`}
+      {`VALUE IS: ${process.env}`}
       <p>Added paragraph line </p>
     </div>
   );
