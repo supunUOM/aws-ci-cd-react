30eded622fae2f38576d6a224bb11dd5167bfa14
supun <supunsandaruwan000000@gmail.com>
2021-11-01 11:46:52 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello from CI/CD app</h1>
-      {`VALUE IS: ${process.env}`}
+      {`VALUE IS: ${process.env.REACT_APP_DISPLAY_VALUE}`}
       <p>Added paragraph line </p>
     </div>
   );
